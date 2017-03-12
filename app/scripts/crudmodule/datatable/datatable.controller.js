d570c020ef64b7fdce93bff89b64795317a59378
Pablo L <Pablo L>
2017-03-12 18:58:04 +0100

    base

--- app/scripts/crudmodule/datatable/datatable.controller.js
+++ app/scripts/crudmodule/datatable/datatable.controller.js
@@ -21,4 +21,22 @@ angular.module('ngCRUDComponents')
       $scope.deleteItem = function(dataObject){
         $scope.$emit('ngCRUDComponents#DatatableCtrl#deleteItem', dataObject);
       }
+
+      $scope.nextPage = function () {
+        if(!($scope.currentPage != null && $scope.maxPage != null) ||
+          $scope.currentPage >= $scope.maxPage)
+          return;
+        $scope.currentPage++;
+        $scope.$emit('ngCRUDComponents#DatatableCtrl#nextPage', $scope.currentPage);
+      }
+
+      $scope.previousPage = function () {
+        if(!($scope.currentPage != null && $scope.maxPage != null) ||
+          $scope.currentPage <= 1)
+          return;
+        $scope.currentPage--;
+        $scope.$emit('ngCRUDComponents#DatatableCtrl#previousPage', $scope.currentPage);
+      }
+
+
   });
